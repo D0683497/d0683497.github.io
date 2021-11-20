3ff4606daf09682f45cc99eb08a4874cac1b2e4a
D0683497 <ff129043923@gmail.com>
2021-11-21 03:46:01 +0800

    部屬

--- .vuepress/navbar.js
+++
@@ -1,7 +0,0 @@
-const { navbarConfig } = require('vuepress-theme-hope');
-
-module.exports = navbarConfig([
-  { text: '首頁', link: '/', icon: 'home' },
-  { text: '部落格', link: '/Blog/' },
-  { text: '黑客社', link: '/HackerSir/' }
-]);
--- Blog/README.md
+++
@@ -1,7 +0,0 @@
----
-home: true
-blog: true
-heroFullScreen: true
-bgImage: https://picsum.photos/2560/1440?grayscale&blur
-heroText: K139 的部落格
----
