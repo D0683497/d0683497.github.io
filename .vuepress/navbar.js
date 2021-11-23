a8a4f27d987148a6b5b33ca396e004fd60280567
D0683497 <ff129043923@gmail.com>
2021-11-23 14:22:51 +0800

    新增 FCU 連結

--- .vuepress/navbar.js
+++ .vuepress/navbar.js
@@ -3,5 +3,6 @@ const { navbarConfig } = require('vuepress-theme-hope');
 module.exports = navbarConfig([
   { text: '首頁', link: '/', icon: 'home' },
   { text: '部落格', link: '/Blog/' },
-  { text: '黑客社', link: '/HackerSir/' }
+  { text: '黑客社', link: '/HackerSir/' },
+  { text: 'FCU', link: 'https://d0683497.github.io/FCU/' }
 ]);
